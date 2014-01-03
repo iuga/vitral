5dcb7daba2aadaeddc8f708b56776cc6fe67566d
Iuga <iugamovil@gmail.com>
2014-01-04 10:42:31 +1300

    [Issue 12] Removing all the unneeded files

--- app/js/directives.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('vitral.directives', ['vitral.services']).
-  directive('vitralGallery', ['appConfig', function(appConfig) {
-    return function(scope, elm, attrs) {
-        console.log(attrs);
-        elm.text(appConfig.photorankUrl);
-    };
-  }]);
